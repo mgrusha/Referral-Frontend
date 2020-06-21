6aac1c24635f8afbdbe045da644f8af8021e4918
mykytagrusha <mykyta.grusha@collibra.com>
2020-06-21 11:05:04 +0200

    Add menu item

--- src/component/navigation/Login.js
+++ src/component/navigation/Login.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import Avatar from "@material-ui/core/Avatar";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 
 const StyledAvatar = styled(Avatar)`
   && {
@@ -11,22 +13,54 @@ const StyledAvatar = styled(Avatar)`
 `;
 
 const User = ({ loggedUser }) => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   let acronym =
     loggedUser.firstName !== "" && loggedUser.lastName !== ""
       ? loggedUser.firstName.substring(0, 1) +
         loggedUser.lastName.substring(0, 1)
       : loggedUser.userName.substring(0, 3);
+  let avatar;
   if (loggedUser.avatar !== "") {
-    console.log(process.env.PUBLIC_URL + `static/avatars/${loggedUser.avatar}`);
-    return (
+    avatar = (
       <StyledAvatar
         alt={loggedUser.userName}
-        src={`../../../static/avatars/${loggedUser.avatar}`}
+        src={process.env.PUBLIC_URL + `/static/avatars/${loggedUser.avatar}`}
+        onClick={handleClick}
       />
     );
   } else {
-    return <StyledAvatar alt={loggedUser.userName}>{acronym}</StyledAvatar>;
+    avatar = (
+      <StyledAvatar alt={loggedUser.userName} onClick={handleClick}>
+        {acronym}
+      </StyledAvatar>
+    );
   }
+
+  return (
+    <div>
+      {avatar}
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>Change settings</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </div>
+  );
 };
 
 export { User };
