00d95cb3fcb9d29494899f0e76dfb0195b661370
mykytagrusha <mykyta.grusha@collibra.com>
2020-07-05 09:14:50 +0200

    Updates

---
+++ precache-manifest.a98904a3343aeba573de3f0981f315b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc7ee4f6e0fab69ca6f1d465c57975c9",
+    "url": "/Referral-Frontend/index.html"
+  },
+  {
+    "revision": "716673d987dea09db0d2",
+    "url": "/Referral-Frontend/static/css/main.710ad1bc.chunk.css"
+  },
+  {
+    "revision": "ccad90bc8ce53ada0307",
+    "url": "/Referral-Frontend/static/js/2.3d64e99d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Referral-Frontend/static/js/2.3d64e99d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "716673d987dea09db0d2",
+    "url": "/Referral-Frontend/static/js/main.99e08fd8.chunk.js"
+  },
+  {
+    "revision": "64fa99483c96b35fc28f",
+    "url": "/Referral-Frontend/static/js/runtime-main.9b86fe33.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Referral-Frontend/precache-manifest.b696ab5d5db7aa31ce67d38ce31672ad.js"
+  "/Referral-Frontend/precache-manifest.a98904a3343aeba573de3f0981f315b9.js"
 );
 
 self.addEventListener('message', (event) => {
